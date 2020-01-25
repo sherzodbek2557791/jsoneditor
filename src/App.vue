7e53ed1885aa3e102a5834ef21b92b13d3d2a88f
test <leon___bek@mail.ru>
2020-01-25 14:58:14 +0500

    added split mechanism

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,6 @@
 <script>
 import JSONEditor from "jsoneditor";
 
-// get json
-// const updatedJson = editor.get();
 export default {
   name: "app",
   data() {
@@ -35,6 +33,7 @@ export default {
   mounted() {
     this.leftCreate();
     this.rightCreate();
+    this.loadMethod();
   },
   methods: {
     onUp() {
@@ -54,6 +53,52 @@ export default {
       const container = document.getElementById("second");
       this.rightEditor = new JSONEditor(container, {});
       this.rightEditor.set(this.jsonData);
+    },
+    loadMethod() {
+      // function is used for dragging and moving
+      function dragElement(element, direction) {
+        let md; // remember mouse down info
+        const first = document.getElementById("first");
+        const second = document.getElementById("second");
+
+        element.onmousedown = onMouseDown;
+
+        function onMouseDown(e) {
+          //console.log("mouse down: " + e.clientX);
+          md = {
+            e,
+            offsetLeft: element.offsetLeft,
+            offsetTop: element.offsetTop,
+            firstWidth: first.offsetWidth,
+            secondWidth: second.offsetWidth
+          };
+          document.onmousemove = onMouseMove;
+          document.onmouseup = () => {
+            //console.log("mouse up");
+            document.onmousemove = document.onmouseup = null;
+          };
+        }
+
+        function onMouseMove(e) {
+          //console.log("mouse move: " + e.clientX);
+          let delta = { x: e.clientX - md.e.x, y: e.clientY - md.e.y };
+
+          if (direction === "H") {
+            // Horizontal
+            // prevent negative-sized elements
+            delta.x = Math.min(
+              Math.max(delta.x, -md.firstWidth),
+              md.secondWidth
+            );
+
+            element.style.left = md.offsetLeft + delta.x + "px";
+            first.style.width = md.firstWidth + delta.x + "px";
+            second.style.width = md.secondWidth - delta.x + "px";
+          }
+        }
+      }
+
+      dragElement(document.getElementById("separator"), "H");
     }
   }
 };
